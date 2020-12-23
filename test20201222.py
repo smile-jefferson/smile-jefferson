28e98e1c726d12a2989f22237e36ac97ed701b19
smile-jefferson <getbacktobusines@hotmail.com>
2020-12-23 10:27:51 +0800

    結果這個裁示昨天創建的檔案，test20201222.py，再傳一次，看會在哪裡出現

--- test20201222.py
+++ test20201222.py
@@ -1,3 +1,7 @@
 test from vscode github
 
 # https://books.bod.idv.tw/2019/10/visual-studio-code-vs-code-git.html
+
+
+git config user.name smile-jefferson
+git config user.email getbacktobusines@hotmail.com
